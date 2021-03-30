c6bc165d344f5c32839805e59bf0d00242a124d9
tillyninjaspace <squealingpet@gmail.com>
2021-03-30 16:23:59 -0700

    before WP test

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -4,17 +4,11 @@ const Loading = () => {
 
     return (
         <div className="loading">
-            
-            {/* <img className="loadingImage" src="loading_slo.gif"/>
-            <p>
-            <span className="loadingText" style={{background: "grey", color: "white", padding: "8px", borderRadius: "5px"}}>Loading Posts...</span>
-            </p> */}
               <div className="loadingImage">
-                  <p>BULLETIN BOARD IS LOADING ...</p>
-              <Spinner animation="border" variant="primary">
+                <p>BULLETIN BOARD IS LOADING...</p>
+                <Spinner animation="border" variant="primary">
                 <span className="sr-only">Loading...</span>
-
-             </Spinner>
+                </Spinner>
              </div>
         </div>
     )
